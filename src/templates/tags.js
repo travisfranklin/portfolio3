fd638fe33efaa2359cdcae33e50ee3818dff161b
travisfranklin <travislf88@gmail.com>
2019-10-14 15:07:00 -0500

    index page + tags update

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -9,7 +9,7 @@ class TagRoute extends React.Component {
     const postLinks = posts.map(post => (
       <li key={post.node.fields.slug}>
         <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
+          <h2 className="">{post.node.frontmatter.title}</h2>
         </Link>
       </li>
     ))
@@ -22,10 +22,10 @@ class TagRoute extends React.Component {
 
     return (
       <Layout>
-        <section className="section">
+        <section className="">
           <Helmet title={`${tag} | ${title}`} />
-          <div className="container content">
-            <div className="columns">
+          <div className="">
+            <div className="">
               <div
                 className="column is-10 is-offset-1"
                 style={{ marginBottom: '6rem' }}
