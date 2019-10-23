f5bd330f11a70d067a2a886caf3895c564c31ab0
travisfranklin <travislf88@gmail.com>
2019-10-23 14:54:33 -0500

    update site title/description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: "Travis' bananas site",
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      "I'm a freelance front-end UI developer and art director specializing in responsive web experiences, modern CSS, and thoughtful, accessible design.",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
