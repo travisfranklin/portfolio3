cbb9fdf74b454f6485188620ce6ce7b04b3a567e
travisfranklin <travislf88@gmail.com>
2019-11-12 12:07:33 -0600

    adjust copy in aside, email in nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -50,7 +50,7 @@ const TemplateWrapper = ({ children }) => {
       </Helmet>
       <div className="linkContainer">
         <ul className="linkList">
-          <li className="linkItem"><a className="link" href="mailto:travislf88@gmail.com">Email</a></li>
+          <li className="linkItem"><a className="link" href="mailto:hello@travislf.com">Email</a></li>
           <li className="linkItem"><a className="link" href="https://www.linkedin.com/in/travisfranklin/">LinkedIn</a></li>
           <li className="linkItem"><a className="link" href="https://github.com/travisfranklin/">GitHub</a></li>
         </ul>
--- src/pages/index.md
+++ src/pages/index.md
@@ -4,6 +4,6 @@ title: Travis Franklin
 subheading: I eat, sleep, and breathe stories. I turn data into empathy, and empathy into design.
 mainpitch:
   description: > 
-    I am a Front End Developer and UI Designer offering over 16 years of experience designing for brands.
+    I am a Front End Developer and UI Engineer offering over 16 years of experience designing for brands.
 cta: I'm currently seeking full-time work. You can reach me at 
 ---
